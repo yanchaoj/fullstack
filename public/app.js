770f87135397b4f5785d42909fa3aa4f5747d195
Yanchao Jin <yanchaoj@gmail.com>
2022-05-24 16:06:50 -0500

    update

--- public/app.js
+++ public/app.js
@@ -5,39 +5,43 @@ searchBtn.addEventListener("click", userinput)
 function userinput() {
     const inputText = document.querySelector('input').value;
     console.log(inputText)
-     $.get(`https://hidden-bastion-86690.herokuapp.com/api/users/${inputText}`, function (data) {
+    $.get(`https://hidden-bastion-86690.herokuapp.com/api/users/${inputText}`, function (data) {
         console.log(data)
         if (inputText === "") {
             alert("Please enter your name!");
         }
+        else if (!data.include(inputText))
+             {
+                alert("User doesn't exist!");
+             }
         else {
 
-            var results = document.querySelector('#results');
-            results.innerHTML = "";
-            for (var i = 0; i < data.length; i++) {
-                var current = data[i];
-                console.log(current);
+        var results = document.querySelector('#results');
+        results.innerHTML = "";
+        for (var i = 0; i < data.length; i++) {
+            var current = data[i];
+            console.log(current);
 
-                var span = document.createElement('span');
+            var span = document.createElement('span');
 
-                var username = document.createElement('div')
-                username.classname = "name";
-                username.textContent = current.name;
-                console.log(username.textContent);
-                span.append(username);
+            var username = document.createElement('div')
+            username.classname = "name";
+            username.textContent = current.name;
+            console.log(username.textContent);
+            span.append(username);
 
-                var task = document.createElement('div')
-                task.classname = "taskcontent";
-                task.textContent = current.task;
-                console.log(task.textContent);
-                span.append(task);
-                
-                results.append(span);
-             
+            var task = document.createElement('div')
+            task.classname = "taskcontent";
+            task.textContent = current.task;
+            console.log(task.textContent);
+            span.append(task);
 
-            }
+            results.append(span);
+
+
         }
-    })
+    }
+})
                  
 
 }

b2d8b00539665ad4863f8077a06634ba84aee8ae
Yanchao Jin <yanchaoj@gmail.com>
2022-05-24 15:26:51 -0500

    update

--- public/app.js
+++ public/app.js
@@ -17,11 +17,12 @@ function userinput() {
             for (var i = 0; i < data.length - 1; i++) {
                 var current = data[i];
                 console.log(current);
-                var span = document.createElement('span');
 
+                var span = document.createElement('span');
                 var username = document.createElement('div')
                 username.classname = "name";
                 username.textContent = current.name;
+                console.log(username);
                 span.append(username);
 
 
@@ -29,6 +30,7 @@ function userinput() {
                 var task = document.createElement('div')
                 task.classname = "taskcontent";
                 task.textContent = current.task;
+                console.log(task);
                 span.append(task);
                 
                 results.append(span);

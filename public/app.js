8d9690af06f908a280c7b69a9350cf7a35578c22
Yanchao Jin <yanchaoj@gmail.com>
2022-05-24 15:08:42 -0500

    update

--- public/app.js
+++ public/app.js
@@ -5,7 +5,7 @@ searchBtn.addEventListener("click", userinput)
 function userinput() {
     const inputText = document.querySelector('input').value;
     console.log(inputText)
-     $.get(`https://hidden-bastion-86690.herokuapp.com/api/users/:name`, function (data) {
+     $.get(`https://hidden-bastion-86690.herokuapp.com/api/users/${inputText}`, function (data) {
         console.log(data)
         if (inputText === "") {
             alert("Please enter your name!");

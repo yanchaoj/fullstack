8281ce051bfd8aebaa49d2f5a0de0c637779d562
Yanchao Jin <yanchaoj@gmail.com>
2022-05-24 11:08:55 -0500

    update

--- public/app.js
+++ public/app.js
@@ -1,6 +1,6 @@
 const searchBtn = document.getElementById('submit')
 
-searchBtn.addEventListener("click", userinput())
+searchBtn.addEventListener("click", userinput)
 function userinput() {
     const inputText = document.querySelector('input').value;
     console.log(inputText)

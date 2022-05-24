cfaff7f643c3add3e296238f629ba3500f94a267
Yanchao Jin <yanchaoj@gmail.com>
2022-05-24 10:51:36 -0500

    update

--- public/app.js
+++ public/app.js
@@ -1,12 +1,41 @@
-const searchBtn=document.getElementById('submit')
+const searchBtn = document.getElementById('submit')
 
-searchBtn.addEventListener("click",userinput)
-function userinput()
-{
+searchBtn.addEventListener("click", userinput)
+function userinput() {
     const inputText = document.querySelector('input').value;
     console.log(inputText)
-    if (inputText === ""){
-        alert("Please input a userID!");
-      }
+    $.get(`https://hidden-bastion-86690.herokuapp.com/${inputText}`, function (data) {
+        console.log(data)
+        if (inputText === "") {
+            alert("Please enter a userID!");
+        }
+        else {
+
+            var results = document.querySelector('#results');
+            results.innerHTML = "";
+            for (var i = 0; i < data.length - 1; i++) {
+                var current = data[i];
+                var span = document.createElement('span');
+
+                var username = document.createElement('div')
+                username.classname = "name";
+                username.textContent = current.userinfo.name;
+                span.append(username);
+
+
+
+                var task = document.createElement('div')
+                task.classname = "taskcontent";
+                task.textContent = current.userino.task;
+                span.append(task);
+
+
+
+            }
+        }
+    })
 
 }
+
+
+
